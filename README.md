7b3fc3cafdcd4e93236b7844c83ca1d3c489c290
talivan00220 <ivancruzhn@gmail.com>
2022-10-31 18:27:23 -0600

    proyecto base con express

---
+++ README.md
@@ -0,0 +1,12 @@
+# Laboratorio 1
+
+## Programacion IV
+## Ivan Cruz
+## 21259
+
+### Instrucciones:
+1. Clonar Proyecto
+2. Instalar dependecias
+    `npm install`
+3. Ejecutar proyecto:
+    `npm start`
---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`servidor HTTP port: ${port}`))
