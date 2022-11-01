5e44d161cf9b190de78ab2f8790aa0b32b7cc069
talivan00220 <ivancruzhn@gmail.com>
2022-10-31 18:37:50 -0600

    estructura de carpetas

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT
 app.get('/', (req, res) => res.send('Hello World!'))
+/*app.get('/empleado', (req,res)=>{
+    res.json({"id":121212,"nombre":"Juan"})
+})*/
 app.listen(port, () => console.log(`servidor HTTP port: ${port}`))
